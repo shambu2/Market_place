8e520f5666bb2b4817d5837516c19f93f3db6d4c
shambu2 <mugalishambu601@gmail.com>
2025-06-13 20:14:21 +0530

    connected to mogo db

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,17 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT = 5000
 import userRoutes from "./routes/user"
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+configDotenv();
 app.get('/',(req:Request,res:Response)=>{
     res.send("this is from backend server of port 5000")
     
 })
 
 app.use('/api/v1',userRoutes)
-
+mongoose.connect(process.env.DB_URL as string)
 app.listen(PORT,()=>{
     console.log(`Server is running in ${PORT}`)
+    console.log(process.env.DB_URL)
 })
