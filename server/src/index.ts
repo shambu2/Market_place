e84864fb46260d4549345c02cd052391e286f922
shambu2 <mugalishambu601@gmail.com>
2025-06-18 10:34:51 +0530

    debug

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,21 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import { configDotenv } from "dotenv";
-import routes from "./routes/index";
+import allRoutes from "./routes/index";
 import cors from 'cors';
+// import MediaDeviceInfo from ''
 const app = express();
 const PORT = 5000;
 configDotenv();
 app.use(cors())
 app.use(express.json())
-app.use("/api/v1",routes)
+app.use("/",allRoutes)
 
 // app.use('/api/v1',userRoutes)
 mongoose.connect(process.env.DB_URL as string)
 app.listen(PORT,()=>{
     console.log(`Server is running in ${PORT}`)
-    console.log(process.env.DB_URL)
+    
+    console.log();
+    
 })
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,22 +1,56 @@
-import express, { Request, Response, Router } from "express";
-const router = Router();
+import express from "express";
+import { Request, Response } from "express";
+// const router = express.Router();
 import bcrypt from "bcrypt";
 import { User } from "../models";
-
-router.get("/", (req, res) => {
-  res.send("hellow from routes");
+import jwt from "jsonwebtoken";
+const router = express.Router();
+router.get("/", async (req: Request, res: Response) => {
+  res.json("hellow from routes");
 });
 
-router.post("/register", async(req: Request, res: Response) => {
+router.post("/register", async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(password, salt);
   try {
-    const newUser = new User({email,password:hashedPassword});
+    const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
-    res.status(200).json({newUser})
-  } catch (error:any) {
-    res.status(500).send('Error while registering User'+ error.message)
+    res.status(200).json({ newUser });
+  } catch (error: any) {
+    res.status(500).send("Error while registering User" + error.message);
+  }
+});
+router.post("/login", async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  try {
+    if (!user) {
+      res.status(401).send("user not exists");
+      return;
+    }
+    const passwordCorrect = bcrypt.compareSync(
+      password,
+      user?.password as string
+    );
+    if (!passwordCorrect) {
+      res.status(404).send("Enter correct Password");
+      return;
+    }
+    const token = jwt.sign({ id: user?.id }, "nothing_is_secret_key", {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.status(200).json({ message: "Login successful" });
+    return;
+  } catch (error) {
+    res.status(401).json("error while logging in ");
+    return;
   }
 });
 
